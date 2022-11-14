ed31feeadc75cacc24dcd651ec3731b56b754616
Bekarion <beqarioni@yahoo.co.uk>
2022-11-14 10:53:38 +0400

    first commit

---
+++ controllers/products.js
@@ -0,0 +1,32 @@
+const Product = require("../models/product")
+
+const getAllProductsStatic = async (req, res, next) => {
+  const search = "tab"
+  const products = await Product.find({
+    name: { $regex: search, $options: "i" },
+  })
+  res.status(200).json({ products, nbHits: products.length })
+}
+
+const getAllProducts = async (req, res, next) => {
+  const { featured, company, name } = req.query
+  const queryObject = {}
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false
+  }
+  if (company) {
+    queryObject.company = company
+  }
+  if (name) {
+    queryObject.name = name
+  }
+
+  const products = await Product.find(queryObject)
+  res.status(200).json({ products, nbHits: products.length })
+}
+
+module.exports = {
+  getAllProducts,
+  getAllProductsStatic,
+}
